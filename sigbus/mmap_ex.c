ad5116549cf2cf62f7885020824cea68afdefa3d
hnjang <hnjang810@gmail.com>
2019-05-09 13:28:30 +0900

    upload mmap_ex.c

---
+++ sigbus/mmap_ex.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+int main() {
+    FILE *f = tmpfile();
+    int *m = mmap(0, 4, PROT_WRITE, MAP_PRIVATE, fileno(f), 0);
+    printf("%s:%d: write 0 to m(%p)\n", __func__, __LINE__, m);
+    *m = 0;
+
+    printf("%s:%d: you reach end of program.\n", __func__, __LINE__);
+}
