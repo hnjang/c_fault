22288c77617c6115953edc8e1eec981dfb097426
hnjang <hnjang810@gmail.com>
2019-05-17 16:59:08 +0900

    update make.py

--- memory_error/heap_corruption/make.py
+++ memory_error/heap_corruption/make.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 
 import sys, os
+import traceback
+
+
+def mkdir_helper(dname):
+    try:
+        os.mkdir(dname)
+    except FileExistsError:
+        pass
+
 
 l_output = ['overrun', 'underrun', 'uaf']
 l_outdir = ['out', 'out-asan']
@@ -9,16 +18,14 @@ if 'CC' in os.environ:
 else:
     cc_bin = 'gcc'
 
-try:
-    for od in l_outdir:
-        os.mkdir(od)
-except:
-    pass
+for od in l_outdir:
+    mkdir_helper(od)
 
 cmds = ['#!/bin/bash']
 for o in l_output:
     cmds.append('{} -g -o {}/{} {}.c'.format(cc_bin, l_outdir[0], o, o))
-    cmds.append('{} -fsanitize=address -g -o {}/{} {}.c'.format(cc_bin, l_outdir[1], o, o))
+    cmds.append('{} -fsanitize=address -g -o {}/{} {}.c'.format(
+        cc_bin, l_outdir[1], o, o))
 with open('tmp.sh', 'w') as f:
     f.write('\n'.join(cmds))
 
